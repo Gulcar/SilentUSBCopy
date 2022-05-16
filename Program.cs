1487ce492ccd4e2dc27d9ea286a541daf151970b
Gulcar <weplaymods@gmail.com>
2022-05-16 19:33:34 +0200

    basic drive read

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Threading;
+
+const uint checkEverySeconds = 5;
+
+while (true)
+{
+    DriveInfo[] drives = DriveInfo.GetDrives();
+
+    foreach (DriveInfo drive in drives)
+    {
+        Console.WriteLine(drive);
+    }
+
+    Thread.Sleep((int)checkEverySeconds * 1000);
+}
